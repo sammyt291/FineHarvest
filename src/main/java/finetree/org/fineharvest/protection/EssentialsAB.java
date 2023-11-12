a3b0551a30424feae07e6d956fb63412eafeec27
SammyT291 <sammyt291@gmail.com>
2023-11-12 12:51:17 +0000

    Added permission definition for autocomplete

--- src/main/java/finetree/org/fineharvest/protection/EssentialsAB.java
+++ src/main/java/finetree/org/fineharvest/protection/EssentialsAB.java
@@ -11,8 +11,9 @@ public class EssentialsAB {
     public static boolean canEssentialsAntiBuild(Player ply, Block b){
 
         Essentials essentials = (Essentials) Bukkit.getServer().getPluginManager().getPlugin("Essentials");
-        User user = essentials.getUser(ply);
+        if(essentials == null){return false;}
 
+        User user = essentials.getUser(ply);
         String blockPerm = "essentials.build.break." + b.getType();
         return user.isAuthorized(blockPerm);
 
